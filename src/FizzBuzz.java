1908c240a504034eecf14069c0a16a65d59aebf1
Carola Lindfors Sejsing <89970224+clindforsSejsing@users.noreply.github.com>
2022-08-25 12:10:17 +0200

    cleaned up code.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,18 +1,18 @@
 public class FizzBuzz {
-   public static void main(String []args){
-      for( int i = 1; i < 101; i++ ){
-         if((i % 3 == 0) && (i % 5 != 0) && i != 42){
-            System.out.println("Fizz");
-         }else if((i % 5 == 0) && (i % 3 !=0)){
-            System.out.println("Buzz");
-         }else if((i % 3 == 0) && (i % 5 == 0)){
+   public static void main(String[] args) {
+      for ( int i = 1; i < 101; i++ ) {
+
+         if ((i % 3 == 0) && (i % 5 == 0)) {
             System.out.println("Fizzbuzz");
-         } else if(i == 42){
+         } else if ((i % 3 == 0) && i != 42) {
+            System.out.println("Fizz");
+         } else if ((i % 5 == 0)) {
+            System.out.println("Buzz");
+         } else if (i == 42) {
             System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
-         } else{
+         } else {
             System.out.println(i);
          }
       }
-
    }
 }
